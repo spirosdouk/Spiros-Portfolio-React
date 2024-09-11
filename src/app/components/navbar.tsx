68b4dd2767daa7467475befcdf505e69272e0357
spirosdouk <s.doukeris@logicea.com>
2024-09-11 14:04:41 +0300

    Initial commit: Setup Next.js project for portfolio

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+    </nav>
+  );
+};
+
+export default Navbar;
